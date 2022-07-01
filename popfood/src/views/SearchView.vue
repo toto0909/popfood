6496e62608a75ed25b64ec9f7cb3ebd7e1d5d770
Yuki Takeda <apple@totonoMacBook-Air.local>
2022-07-01 22:11:28 +0900

    router設定/不要ファイル削除

---
+++ popfood/src/views/SearchView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <p>検索</p>
+        <router-link active-class="link-active" exact :to="{name:'weather'}">
+            <span>天気</span>
+        </router-link>
+    </div>
+</template>
